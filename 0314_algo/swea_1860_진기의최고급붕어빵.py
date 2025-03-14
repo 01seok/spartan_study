b941a74cb328c3e0f42a96e1c2c6df4a3d953cc0
LEESEOKJAE <lsj6924@naver.com>
2025-03-15 02:43:34 +0900

    swea_1860_진기의최고급붕어빵.py

--- 0314_algo/swea_1860_진기의최고급붕어빵.py
+++ 0314_algo/swea_1860_진기의최고급붕어빵.py
@@ -3,15 +3,15 @@ for tc in range(1, T + 1):
     N, M, K = map(int, input().split())
     guest_time = sorted(list(map(int, input().split())))
     
-    current_fish = 0
-    guest_num = 0
-    flag = True
+    current_fish = 0    # 현재 보유 중인 붕어빵 수
+    guest_num = 0       # 손님 번호(인덱스로 쓸)
+    flag = True         # flag변수 impossible 나오면 탈출용
     
     # 0초부터 마지막 손님 오는 시간까지 반복문
     for time in range(guest_time[-1] + 1):
         # 0초에는 붕어빵 없음, M초마다 K개의 붕어빵 생산
-        if time > 0 and time % M == 0:
-            current_fish += K
+        if time > 0 and time % M == 0:  # 나눴을 때 나머지 없다 => 입력으로 주어진 붕어빵 생성 시간이다
+            current_fish += K   # k개 보유 중인 붕어빵에 추가
         
         # 손님 숫자가 오기로 한 N명 일 때 까지 while문, 손님 오기로 한 시간과 지금 시간이 일치할 때 실행
         while guest_num < N and guest_time[guest_num] == time:
@@ -19,7 +19,7 @@ for tc in range(1, T + 1):
                 print(f'#{tc} Impossible')
                 flag = False
                 break
-            if current_fish >= 0:
+            if current_fish >= 0:   # 붕어빵 남아있다면
                 current_fish -= 1   # 붕어빵 한 마리 빼고
                 guest_num += 1  # 다음 손님 받으러 가기
             
